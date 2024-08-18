8815ea0ca8747fcf38c47c2f48de3e8fd6899f7b
martinHype <batora.martin33@gmail.com>
2024-08-18 23:19:21 +0200

    adding the authentication

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 
     use Illuminate\Support\Facades\Route;
+    use Illuminate\Http\Request;
 
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 
 
 
